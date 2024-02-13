5da242e9f7750e36c30af88a6771710c08d3b4e2
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-02-13 20:02:46 +0000

    2 updated and 2 deletes left

--- GoodsBDextra/GoodsOperationsWindow.xaml.cs
+++ GoodsBDextra/GoodsOperationsWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,25 +12,198 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace GoodsBDextra
 {
-
+    /// <summary>
+    /// Interaction logic for GoodsOperationsWindow.xaml
+    /// </summary>
     public partial class GoodsOperationsWindow : Window
     {
-        public GoodsOperationsWindow()
+
+        bool Edit;
+        int Id;
+        string Name, Date, Cost, Quantity, GoodType, Supplier;
+        public GoodsOperationsWindow(bool Edit = false, int id = 0, string name = "Test", string date = "", string cost = "", string quantity = "", string goodtype = "", string supplier = "")
         {
             InitializeComponent();
+
+            this.Edit = Edit;
+            this.Id = id;
+            Name = name;
+            Date = date;
+            Cost = cost;
+            Quantity = quantity;
+            GoodType = goodtype;
+            Supplier = supplier;
+
+            txtName.Text = name;
+            dpDate.Text = date;
+            txtCost.Text = cost;
+            txtQuantity.Text = quantity;
+
+
+            SqlConnection connect = new SqlConnection(@"Data Source=PECHKA\SQLEXPRESS;Initial Catalog=Storage;Integrated Security=True");
+            SqlCommand command = new SqlCommand();
+
+            try
+            {
+                connect.Open();
+                command.Connection = connect;
+                command.CommandText = "Select Name from GoodType;";
+                SqlDataReader reader = command.ExecuteReader();
+
+                cmbGoodType.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string typeName = reader["Name"].ToString();
+                    cmbGoodType.Items.Add(typeName);
+                }
+
+                reader.Close();
+
+                command.CommandText = "Select Name from Supplier;";
+                reader = command.ExecuteReader();
+
+                cmbSupplier.Items.Clear();
+
+                while (reader.Read())
+                {
+                    string typeName = reader["Name"].ToString();
+                    cmbSupplier.Items.Add(typeName);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                command.Dispose();
+                connect.Close();
+            }
+            cmbGoodType.SelectedIndex = 0;
+            cmbSupplier.SelectedIndex = 0;
         }
 
         private void OK_Click(object sender, RoutedEventArgs e)
         {
+            SqlConnection connect = new SqlConnection(@"Data Source=PECHKA\SQLEXPRESS;Initial Catalog=Storage;Integrated Security=True");
+            SqlCommand command = new SqlCommand();
+            try
+            {
+                if (ValidateInput())
+                {
+                    connect.Open();
+                    command.Connection = connect;
 
+                    int supplierID = GetIDByName("Supplier", cmbSupplier.SelectedItem.ToString());
+                    int goodTypeID = GetIDByName("GoodType", cmbGoodType.SelectedItem.ToString());
+
+                    if (!Edit)
+                    {
+                        command.CommandText = $"INSERT INTO Goods (Name, DeliveryDate, Cost, Quantity, TypeID, SupplierID)" +
+                                              $"VALUES ('{txtName.Text}', '{dpDate.Text}', {Convert.ToDouble(txtCost.Text)}, {Convert.ToInt32(txtQuantity.Text)}, '{goodTypeID}', '{supplierID}')";
+                        int n = command.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        command.CommandText = $"UPDATE Goods SET " +
+                      $"Name = '{txtName.Text}', " +
+                      $"DeliveryDate = '{dpDate.Text}', " +
+                      $"Cost = {Convert.ToDouble(txtCost.Text)}, " +
+                      $"Quantity = {Convert.ToInt32(txtQuantity.Text)}, " +
+                      $"TypeID = '{goodTypeID}', " +
+                      $"SupplierID = '{supplierID}' " +
+                      $"WHERE ID = {Id}";
+                        int n = command.ExecuteNonQuery();
+                    }
+
+                    connect.Close();
+                }
+               
+ 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                command.Dispose();
+                connect.Close();
+            }
+
+            DialogResult = true;
+            Close();
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
+            DialogResult = false;
+            Close();
+        }
 
+        private int GetIDByName(string tableName, string name)
+        {
+            int id = -1; 
+
+            using (SqlConnection connect = new SqlConnection(@"Data Source=PECHKA\SQLEXPRESS;Initial Catalog=Storage;Integrated Security=True"))
+            {
+                connect.Open();
+
+                string query = $"SELECT ID FROM {tableName} WHERE Name = @Name;";
+                using (SqlCommand idCommand = new SqlCommand(query, connect))
+                {
+                    idCommand.Parameters.AddWithValue("@Name", name);
+
+                    try
+                    {
+                        object result = idCommand.ExecuteScalar();
+                        if (result != null)
+                        {
+                            id = Convert.ToInt32(result);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+
+            return id;
+        }
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrEmpty(txtName.Text) ||
+                string.IsNullOrEmpty(dpDate.Text) ||
+                string.IsNullOrEmpty(txtCost.Text) ||
+                string.IsNullOrEmpty(txtQuantity.Text) ||
+                cmbGoodType.SelectedItem == null ||
+                cmbSupplier.SelectedItem == null)
+            {
+                MessageBox.Show("Please fill in all the fields.");
+                return false;
+            }
+
+            if (!DateTime.TryParse(dpDate.Text, out _))
+            {
+                MessageBox.Show("Invalid date format.");
+                return false;
+            }
+
+            if (!double.TryParse(txtCost.Text, out _) || !int.TryParse(txtQuantity.Text, out _))
+            {
+                MessageBox.Show("Invalid numeric values for Cost or Quantity.");
+                return false;
+            }
+
+            return true;
         }
     }
 }
--- GoodsBDextra/MainWindow.xaml.cs
+++ GoodsBDextra/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
 using System.Windows.Controls.Primitives;
+using System.Collections;
 
 namespace GoodsBDextra
 {
@@ -133,9 +134,16 @@ namespace GoodsBDextra
 
         private void MenuItemCreateNew_Click(object sender, RoutedEventArgs e)
         {
-
             GoodsOperationsWindow create_goods = new GoodsOperationsWindow();
             create_goods.ShowDialog();
+            if (create_goods.DialogResult == true)
+            {
+                MenuItemShowAll_Click(sender, e);
+            }
+            else if (create_goods.DialogResult == false)
+            {
+                return;
+            }
         }
 
         private void MenuItemCreateNewType_Click(object sender, RoutedEventArgs e)
@@ -170,7 +178,38 @@ namespace GoodsBDextra
 
         private void MenuItemUpdateGoods_Click(object sender, RoutedEventArgs e)
         {
-            // TODO: Реализовать логику для "Update Goods"
+            DataGrid selectedDataGrid = datagrid; 
+            var selectedItems = selectedDataGrid.SelectedItems;
+
+            if (selectedItems.Count == 0)
+            {
+                MessageBox.Show("Choose a good first!");
+                return;
+            }
+
+            var selectedRow = selectedItems[0] as DataRowView; 
+
+            if (selectedRow != null)
+            {
+                int id = (int)selectedRow["ID"]; 
+                string name = selectedRow["GoodName"].ToString();
+                string date = selectedRow["DeliveryDate"].ToString();
+                string cost = selectedRow["Cost"].ToString();
+                string quantity = selectedRow["Quantity"].ToString();
+                string type = selectedRow["TypeName"].ToString();
+                string supplier = selectedRow["SupplierName"].ToString();
+
+                GoodsOperationsWindow create_goods = new GoodsOperationsWindow(true,id,name,date,cost,quantity,type,supplier);
+                create_goods.ShowDialog();
+                if (create_goods.DialogResult == true)
+                {
+                    MenuItemShowAll_Click(sender, e);
+                }
+                else if (create_goods.DialogResult == false)
+                {
+                    return;
+                }
+            }
         }
 
         private void MenuItemUpdateType_Click(object sender, RoutedEventArgs e)
@@ -185,7 +224,46 @@ namespace GoodsBDextra
 
         private void MenuItemDeleteGoods_Click(object sender, RoutedEventArgs e)
         {
-            // TODO: Реализовать логику для "Delete Goods"
+            DataGrid selectedDataGrid = datagrid;
+            var selectedItems = selectedDataGrid.SelectedItems;
+
+            if (selectedItems.Count == 0)
+            {
+                MessageBox.Show("Choose a good first!");
+                return;
+            }
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to delete this?", "Attention!", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            if (result != MessageBoxResult.OK)
+            {
+                return;
+            }
+
+            SqlConnection connect = new SqlConnection(@"Data Source=PECHKA\SQLEXPRESS;Initial Catalog=Storage;Integrated Security=True");
+            SqlCommand command = new SqlCommand();
+            try
+            {
+                connect.Open();
+                command.Connection = connect;
+
+                var selectedRow = selectedItems[0] as DataRowView;
+
+                if (selectedRow != null)
+                {
+                    int id = (int)selectedRow["ID"];
+                    CreateQuery($"DELETE FROM Goods WHERE id = {id}");
+                    MenuItemShowAll_Click(sender, e);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                command.Dispose();
+                connect.Close();
+            }
         }
 
         private void MenuItemDeleteType_Click(object sender, RoutedEventArgs e)
